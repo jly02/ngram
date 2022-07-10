3f9040051b2118f0b94fa0a9b11df493654c28a0
jly02 <lyjonathan02@gmail.com>
2022-07-10 11:31:48 -0700

    Iterator through files

--- ngrams.cpp
+++ ngrams.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main(void) {
     int n;
@@ -10,9 +13,16 @@ int main(void) {
     if(!file) {
         std::cerr << "Could not open the file!" << std::endl;
     } else {
+        // testing area
         while(getline(file, text)) {
             std::cout << text << std::endl;
         }
+
+        /* FOR ITERATING THROUGH EACH FILE IN OANC DIRECTORY */
+        // std::string path = "./oanc";
+        // for (const auto & entry : fs::directory_iterator(path)) {
+        //     std::cout << entry.path() << std::endl;
+        // }
     }
 
     file.close();
