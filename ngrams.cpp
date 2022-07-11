d7c08145bfc09376b36380ac6201ce04a0492282
jly02 <lyjonathan02@gmail.com>
2022-07-10 21:11:35 -0700

    Implemented trigram method partially

--- ngrams.cpp
+++ ngrams.cpp
@@ -3,18 +3,15 @@
 #include <string>
 #include <filesystem>
 #include <vector>
+#include <map>
 
 namespace fs = std::filesystem;
+using index_t = std::vector<int>::size_type;
 using std::string;
 using std::vector;
+using std::map;
 using std::cout;
 
-/**
- * @brief Splits a given string into words by space.
- * 
- * @param text the text to be split
- * @return vector<string> a vector of the words after being split
- */
 vector<string> split(string text) {
     string space_delimiter = " ";
     vector<string> words{};
@@ -28,10 +25,27 @@ vector<string> split(string text) {
     return words;
 }
 
+void trigrams(vector<string> words) {
+    vector<string> triwords{};
+    if(words.size() < 3) {
+        return;
+    }
+
+    for(index_t i = 0; i < words.size() - 2; i++) {
+        // put three words together
+        triwords.push_back(words.at(i) + " " + words.at(i + 1) + " " + words.at(i + 2));
+    }
+
+    for(const auto& word : triwords) {
+        cout << word << '\n';
+    }
+}
+
 int main(void) {
     int n;
     string text;
     std::ifstream file("./oanc/1-3_meth_901.txt");
+    map<string, int> freq{};
 
     // Check for error opening file
     if(!file) {
@@ -43,11 +57,8 @@ int main(void) {
         // }
 
         while(getline(file, text)) {
-            vector<string> splitWords = split(text);
-
-            for(const auto& word : splitWords) {
-                cout << word << '\n';
-            }
+            vector<string> splitWords = split(text + " ");
+            trigrams(splitWords);
         }
 
         /* FOR ITERATING THROUGH EACH FILE IN OANC DIRECTORY */
