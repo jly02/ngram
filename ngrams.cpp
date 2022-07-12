cdb618777dac85ca1c74c76f7a248b41a66f3eff
jly02 <lyjonathan02@gmail.com>
2022-07-11 21:38:23 -0700

    Added methods, early implementation of predict()

--- ngrams.cpp
+++ ngrams.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <time.h>
+#include <stdlib.h>
 
 namespace fs = std::filesystem;
 using index_t = std::vector<int>::size_type;
@@ -16,9 +18,26 @@ using std::map;
 using std::cout;
 
 class NGramModel {
+  private:
+    map<string, set<string>> context_cand;
+    map<pair<string, string>, int> ngram_counter;
+
+    int range_rand(int min, int max){
+        int n = max - min + 1;
+        int remainder = RAND_MAX % n;
+        int x;
+
+        do {
+            x = rand();
+        } while(x >= RAND_MAX - remainder);
+
+        return min + x % n;
+    }
+
   public:
     NGramModel() {
-        context = {};
+        srand(time(NULL));
+        context_cand = {};
         ngram_counter = {};
     }
 
@@ -30,14 +49,28 @@ class NGramModel {
         for(index_t i = 0; i < words.size() - 2; i++) {
             // tuple of ({w - 2} + {w - 1}, w) 
             string key = words.at(i) + " " + words.at(i + 1);
-            this->context[key].insert(words.at(i + 2));
+            this->context_cand[key].insert(words.at(i + 2));
             this->ngram_counter[std::make_pair(key, words.at(i + 2))]++;
         }
     }
 
     // Not yet implemented.
     string predict(string context) {
-        return NULL;
+        set<string> possible_words = this->context_cand[context];
+        // int numContext = ngram_counter[std::make_pair(context, "")];
+        index_t rand_i = range_rand(0, possible_words.size() - 1);
+
+        set<string>::iterator it = possible_words.begin();
+        std::advance(it, rand_i);
+        return *(it);
+    }
+
+    string gen_sentence(size_t length) {
+        // to be implemented
+    }
+
+    string ngram_prob(string context) {
+        // to be implemented
     }
 
     /**
@@ -84,10 +117,6 @@ class NGramModel {
             cout << '\'' << std::get<0>(key) << "', '" << std::get<1>(key) << "' => " << val << '\n';
         }
     }
-
-  private:
-    map<string, set<string>> context;
-    map<pair<string, string>, int> ngram_counter;
 };
 
 int main(void) {
@@ -112,7 +141,8 @@ int main(void) {
             model.update_model(words);
         }
 
-        model.print_ngrams();
+        // model.print_ngrams();
+        cout << model.predict("would be") << '\n';
 
         /* FOR ITERATING THROUGH EACH FILE IN OANC DIRECTORY */
         // string path = "./oanc";
